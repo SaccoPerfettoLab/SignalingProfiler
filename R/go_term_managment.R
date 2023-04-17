c4554a4a0099097c24531485d6b3aeb5d25e13ec
veronicavenafra <77729333+veronicavenafra@users.noreply.github.com>
2023-04-17 15:02:03 +0200

    new molecular function annotation

--- R/go_term_managment.R
+++ R/go_term_managment.R
@@ -3,12 +3,12 @@
 #' Title
 #'
 #' @param inferred_proteins_dataset dataset of inferred proteins with UNIPROT
-#'
+#' @param organism string, 'mouse', or 'human'
 #' @return dataset of inferred proteins with molecular function column
 #' @export
 #'
 #' @examples
-molecular_function_annotation <- function(inferred_proteins_dataset){
+molecular_function_annotation <- function(inferred_proteins_dataset, organism){
 
   # ADD A CHECK FOR UNIPROT COLUMN PRESENCE
   inferred_proteins_dataset <- inferred_proteins_dataset %>%
@@ -19,41 +19,21 @@ molecular_function_annotation <- function(inferred_proteins_dataset){
   #get uniprot annotation
   inferred_proteins_dataset_f <- inferred_proteins_dataset %>%
     dplyr::filter(!grepl('*SIGNOR*', UNIPROT))
-  annotation <- UniprotR::GetProteinGOInfo(inferred_proteins_dataset_f$UNIPROT)
 
-  # extract GO term for each protein
-  ovl <- annotation$Gene.Ontology..molecular.function.
-  pattern <- "(\\[.*?\\])"
-  matches <- gregexpr(pattern, ovl)
-  overlap <- regmatches(ovl, matches)
-  GOterm <- lapply(overlap, function(x){stringr::str_sub(x, 2,-2)})
 
-  # set ontology as mf
-  GOSim::setOntology(ont = "MF", loadIC=TRUE, DIR=NULL)
-
-  # get ancestors for each GO term
-  GOanc <- GOSim::getAncestors()
-
-  inferred_proteins_dataset_f$mf <- NA
-  for(i in c(1:length(inferred_proteins_dataset_f$gene_name))){
-
-    # transcription factor
-    if(sum(unlist(lapply(GOterm[[i]], function(x){'GO:0140110' %in% GOanc[[x]] |
-        'GO:0140110' == x})))>=1){
-      inferred_proteins_dataset_f$mf[i] <- 'tf'
-      # kinase
-    }else if(sum(unlist(lapply(GOterm[[i]], function(x){'GO:0016301' %in% GOanc[[x]] |
-        'GO:0016301' == x})))>=1){
-      inferred_proteins_dataset_f$mf[i] <- 'kin'
-      # phosphatase
-    }else if(sum(unlist(lapply(GOterm[[i]], function(x){'GO:0004725' %in% GOanc[[x]] |
-        'GO:0004725' == x})))>=1){
-      inferred_proteins_dataset_f$mf[i] <- 'phos'
-      # other types
-    }else{
-      inferred_proteins_dataset_f$mf[i] <- 'other'
-    }
+  if(organism == 'human'){
+    inferred_proteins_dataset_f <- dplyr::left_join(inferred_proteins_dataset_f,
+                                                    gomf_human,
+                                                    by = c('UNIPROT'))
+  }else if(organism == 'mouse' | organism == 'hybrid'){
+    inferred_proteins_dataset_f <- dplyr::left_join(inferred_proteins_dataset_f,
+                                                    gomf_mouse,
+                                                    by = c('UNIPROT'))
+  }else{
+    stop('please provide a valid organism')
   }
+
+  inferred_proteins_dataset_f$mf[is.na(inferred_proteins_dataset_f$mf)] <- 'other'
 
   missing_genes <- inferred_proteins_dataset %>%
     dplyr::filter(!gene_name %in% inferred_proteins_dataset_f$gene_name)
@@ -67,3 +47,4 @@ molecular_function_annotation <- function(inferred_proteins_dataset){
 
   return(inferred_proteins_dataset)
 }
+
