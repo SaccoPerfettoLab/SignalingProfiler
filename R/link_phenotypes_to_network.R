1bd2bbe56e372da25ae9832e4abf8a5635d37bff
veronicavenafra <veronica.venafra97@gmail.com>
2025-03-20 14:02:21 +0100

    fixed phenoscore error

---
+++ R/link_phenotypes_to_network.R
@@ -0,0 +1,111 @@
+#' Link Phenotypes to the Signaling Network
+#'
+#' This function integrates phenotype regulatory relationships into the
+#' existing signaling network from SignalingProfiler, creating an extended
+#' network with phenotype nodes and phenotype-regulator edges.
+#'
+#' @param phenotype_regulators A dataframe containing phenotype-regulator
+#'   interactions with columns `EndPathways`, `Effect`, and `regulators` (Gene Symbol).
+#' @param phenoscore_df A dataframe containing computed phenoscores for
+#'   each phenotype, including `EndPathways` and `phenoscore`.
+#' @param sp_graph An `igraph` object representing the signaling network
+#'   from SignalingProfiler network generation step.
+#'
+#' @return A list containing:
+#'   \item{igraph_network}{An `igraph` object representing the extended network, including phenotype nodes.}
+#'   \item{nodes_df}{A dataframe containing node attributes.}
+#'   \item{edges_df}{A dataframe containing edge relationships, including phenotype-regulator interactions.}
+#'
+#' @details
+#' This function expands the existing signaling network by:
+#' - Extracting regulators for each phenotype.
+#' - Creating phenotype nodes with computed phenoscores.
+#' - Connecting phenotype nodes to their associated regulators.
+#' - Merging the new nodes and edges with the existing `sp_graph`.
+#'
+#' The resulting network allows for the analysis of how protein regulators
+#' influence phenotypic pathways.
+#'
+#' @examples
+#' library(igraph)
+#' library(dplyr)
+#'
+#' # Example phenotype regulator data
+#' phenotype_regulators <- data.frame(
+#'   EndPathways = c("Apoptosis", "Cell Growth"),
+#'   Effect = c("up-regulates", "down-regulates"),
+#'   regulators = c("P53;AKT1", "MAPK1")
+#' )
+#'
+#' # Example phenoscore dataframe
+#' phenoscore_df <- data.frame(
+#'   EndPathways = c("Apoptosis", "Cell Growth"),
+#'   phenoscore = c(2.5, -1.8)
+#' )
+#'
+#' # Example signaling network
+#' example_graph <- graph_from_edgelist(
+#'   matrix(c("P53", "AKT1", "AKT1", "MAPK1"), ncol = 2, byrow = TRUE),
+#'   directed = TRUE
+#' )
+#'
+#' # Generate phenotype-augmented network
+#' sp_object <- link_phenotypes_to_network(phenotype_regulators, phenoscore_df, example_graph)
+link_phenotypes_to_network <- function(phenotype_regulators, phenoscore_df, sp_graph){
+
+  # Extract phenotypes regulators
+  phenotype_regulators <- results.table_reg %>%
+    dplyr::select(EndPathways, Effect, regulators) %>%
+    tidyr::separate_rows(regulators, sep = ';') %>%
+    dplyr::mutate(Effect = ifelse(Effect == 'down-regulates', -1, 1)) %>%
+    dplyr::rename('source' = 'regulators' , 'sign' = 'Effect', 'target' = 'EndPathways')
+
+  # Create a phenotype table in SignalingProfiler compliant format
+  pheno_nodes <- tidyr::tibble(gene_name =  phenoscore_df$EndPathways,
+                               carnival_activity = NA,
+                               UNIPROT = NA,
+                               mf = 'phenotype',
+                               final_score = phenoscore_df$phenoscore,
+                               method = 'phenoscore',
+                               discordant = FALSE
+  )
+
+  pheno_nodes <- pheno_nodes %>%
+    dplyr::mutate(carnival_activity = ifelse(final_score < 0, -100, 100),
+                  gene_name = stringr::str_replace_all(str_to_upper(gene_name), "[^[:alnum:]]", '_'))
+
+  # Get SignalingProfiler network proteins
+  nodes_df <- igraph::as_data_frame(sp_graph, what = 'vertices')
+  colnames(nodes_df)[1] <- 'gene_name'
+
+  # Create a nodes table
+  node_df_pheno <- dplyr::bind_rows(nodes_df, pheno_nodes) %>%
+    dplyr::distinct()
+
+  pheno_edges <- phenotype_regulators %>% dplyr::mutate(target = stringr::str_replace_all(str_to_upper(target), "[^[:alnum:]]", '_'))
+  pheno_edges <- tidyr::tibble(target = pheno_edges$target,
+                               sign = as.character(pheno_edges$sign),
+                               source = pheno_edges$source,
+                               carnival_weight = 100,
+                               direct = 'FALSE',
+                               aminoacid = '',
+                               is_quantified = 'FALSE',
+                               is_significant = 'FALSE',
+                               FC = '' )
+
+  # Get SignalingProfiler network edges
+  edges_df <- tidyr::as_tibble(igraph::as_data_frame(sp_graph, what = 'edges'))
+  colnames(edges_df)[1:2] <- c('source', 'target')
+  edges_df$sign <- as.character(edges_df$sign)
+  edges_df_pheno <- dplyr::bind_rows(edges_df, pheno_edges)
+
+  # Create igraph object
+  pheno_graph <- igraph::graph_from_data_frame(edges_df_pheno,
+                                               vertices = node_df_pheno %>% dplyr::distinct())
+
+  sp_object <- list(igraph_network = pheno_graph,
+                    nodes_df = node_df_pheno,
+                    edges_df = edges_df_pheno)
+
+  return(sp_object)
+}
---
+++ R/optimize_pheno_network.R
@@ -0,0 +1,87 @@
+#' Optimize Phenotype Network with CARNIVAL
+#'
+#' This function optimizes the protein-to-phenotype network by retrieving only
+#' sign-coherent protein-to-phenotype edges with phenotypes activity.
+#' Optionally it calls [expand_and_map_edges()] to map edges using phosphoproteomics data.
+#'
+#' @param sp_object A list containing the SignalingProfiler network, nodes, and edges.
+#' @param organism A character string specifying the organism (`"human"` or `"mouse"`).
+#' @param phospho_df An optional tibble containing phosphoproteomics data. Default is `NULL`.
+#' @param carnival_options A list of options returned by `default_CARNIVAL_options()`.
+#' @param files Logical, whether to generate output files (`TRUE` or `FALSE`).
+#' @param direct Logical, whether to use only direct interactions (`TRUE` or `FALSE`, default: `FALSE`).
+#' @param with_atlas Logical, whether to include Kinome Atlas regulons (`TRUE` or `FALSE`, default: `TRUE`).
+#' @param path_sif A character string specifying the path for the SIF output file. Default is `'./pheno_opt_graph.sif'`.
+#' @param path_rds A character string specifying the path for the RDS output file. Default is `'./pheno_opt_graph.rds'`.
+#'
+#' @return A modified SP object containing the optimized phenotype network.
+#'
+#' @details
+#' This function:
+#' - Extracts phenotype and non-phenotype nodes from the input network.
+#' - Prepares the edge list in SIF format.
+#' - Runs CARNIVAL-based network optimization.
+#' - If phosphoproteomics data is provided, maps validated edges.
+#' - Returns the updated SP object with the optimized phenotype network.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' data('toy_sp_object')
+#' carnival_options <- default_CARNIVAL_options(solver = "cplex")
+#' optimize_pheno_network(sp_object = toy_sp_object, organism = 'human', carnival_options)
+#' }
+optimize_pheno_network <- function(sp_object,
+                                   organism,
+                                   phospho_df = NULL,
+                                   carnival_options,
+                                   files,
+                                   direct = FALSE,
+                                   with_atlas = FALSE,
+                                   path_sif = './pheno_opt_graph.sif',
+                                   path_rds = './pheno_opt_graph.rds'){
+
+  # Extract phenotype and non-phenotype nodes
+  start_df <- sp_object$sp_object_phenotypes$nodes_df %>%
+    dplyr::filter(mf != 'phenotype') %>%
+    dplyr::select(UNIPROT, gene_name, final_score = carnival_activity, mf, method)
+
+  pheno_df <- sp_object$sp_object_phenotypes$nodes_df %>%
+    dplyr::filter(mf == 'phenotype') %>%
+    dplyr::select(UNIPROT, gene_name, final_score = carnival_activity, mf, method)
+
+  # Transform edges into SIF format
+  pheno_naive_df <- sp_object$sp_object_phenotypes$edges_df %>%
+    dplyr::select(source, interaction = sign, target)
+
+  # Run CARNIVAL optimization
+  pheno_out <- run_carnival_and_create_graph(source_df = start_df,
+                                             target_df = pheno_df,
+                                             naive_network = unique(pheno_naive_df),
+                                             proteins_df = sp_object$sp_object_phenotypes$nodes_df %>%
+                                               dplyr::select(UNIPROT, gene_name, final_score, mf, method),
+                                             organism = organism,
+                                             carnival_options = carnival_options,
+                                             files = files,
+                                             direct = direct,
+                                             with_atlas = with_atlas,
+                                             path_sif = path_sif,
+                                             path_rds = path_rds)
+
+  # Validate network with phosphoproteomics if provided
+  sp_object$sp_object_phenotypes <- if(!is.null(phospho_df)){
+    expand_and_map_edges(optimized_object = pheno_out,
+                         organism = organism,
+                         phospho_df = phospho_df,
+                         files = files,
+                         direct = direct,
+                         with_atlas = with_atlas,
+                         path_sif = path_sif,
+                         path_rds = path_rds)
+  }else{
+    pheno_out
+  }
+
+  return(sp_object)
+}
